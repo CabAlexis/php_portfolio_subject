fb6ca962e6a07408c4d864be6b57e208d8d81c0f
CabAlexis <alexis.cabillic@gmail.com>
2020-11-24 19:14:36 +0100

    page présentation faite

--- src/templates/header.tpl.php
+++ src/templates/header.tpl.php
@@ -9,20 +9,23 @@
   <!-- Déclaration de notre font -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800" rel="stylesheet">
 
+  <!-- Google Font -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Texturina:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
   <!-- Font-Awesome CSS -->
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
-    integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="assets/css/nav.style.css">
+  <link rel="stylesheet" href="assets/css/style.css">
 
 
   <title>Portfolio Php Training</title>
 </head>
 
 <body class="text-center">
-<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+  <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
 
-    <?php require __DIR__.'/../templates/nav.tpl.php'; ?>
+    <?php require __DIR__ . '/../templates/nav.tpl.php'; ?>
